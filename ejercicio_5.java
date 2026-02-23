b81a32a8a2b9aed1e3899db28637773d0107f310
johancogollo76-cloud <johancogollo76@gmail.com>
2026-02-23 10:41:19 -0500

    imc

---
+++ ejercicio_5.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class ejercicio_5 {
+    public static void main(String[] args) {
+        Scanner tc = new Scanner(System.in);
+        System.err.println("escrba el nombre: ");
+        String nombre = tc.nextLine();
+
+        System.err.println(" escriba el peso en KG: ");
+        double peso = tc.nextDouble();
+
+        System.err.println("escriba la altura en M: ");
+        double altura = tc.nextDouble();
+
+        double imc = peso/( altura *altura);
+
+        if (imc < 18.5){
+            System.err.println(nombre);
+            System.err.println(imc);
+            System.err.println("BAJO DE PESO");
+            
+        }
+        else if (imc <= 18.5 && imc <25) {
+            System.err.println(nombre);
+            System.err.println(imc);
+            System.err.println("NORMAL");   
+        }
+        else if (imc <=25 && imc < 30){
+            System.err.println(nombre);
+            System.err.println(imc);
+            System.err.println("SOBREPESO");
+        }
+        else if (imc >= 30 ){
+            System.err.println(nombre);
+            System.err.println(imc);
+            System.err.println("OBESIDAD");
+        }
+
+
+        tc.close();
+    }
+    
+}
