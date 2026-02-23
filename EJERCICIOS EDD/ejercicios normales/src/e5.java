985d133dd23b21be9f8cee8ae04ceb055305fc56
johancogollo76-cloud <johancogollo76@gmail.com>
2026-02-23 15:41:57 -0500

    actualizacion

---
+++ EJERCICIOS EDD/ejercicios normales/src/e5.java
@@ -0,0 +1,16 @@
+public class e5 {
+    public static void main(String[] args) {
+        double tazas;
+        double mililitros;
+        double litros;
+
+        tazas = 2.5;
+        mililitros = tazas * 236.588;
+        litros = mililitros / 1000;
+
+        System.out.println("Tazas: " + tazas);
+        System.out.println("Mililitros: " + mililitros);
+        System.out.println("Litros: " + litros);
+    }
+    
+}
