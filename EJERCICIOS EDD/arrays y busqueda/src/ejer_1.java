185dc0ebb4806cad663c92140fcd334bc3b203e9
johancogollo76-cloud <johancogollo76@gmail.com>
2026-03-02 21:27:22 -0500

    update

--- EJERCICIOS EDD/arrays y busqueda/src/ejer_1.java
+++ EJERCICIOS EDD/arrays y busqueda/src/ejer_1.java
@@ -2,14 +2,11 @@
 
 // El Problema: Debes recorrer la lista para verificar si el producto existe y en qué posición de la estantería se encuentra.
 // Algoritmo a usar: Búsqueda Lineal. Es el ideal porque los productos en la estantería no tienen un orden numérico específico.
-import java.util.Scanner;
 public class ejer_1 {
     public static void main(String[] args) throws Exception {
-        Scanner tc = new Scanner(System.in);
         boolean encontrado = false;
         double [] codigos = {23240,23424,234636,12313,45645,123123};
-        System.out.println("ingresa el codigo del producto que deseas buscar: ");
-        int codigo = tc.nextInt();
+        int codigo = 234636;
         for (int i =0;i<codigos.length;i++) {
             if (codigos[i]==codigo) {
                 System.out.println("el valor," +codigos[i]+ " se encuentra dentro del array y esta en la posicion: "+i);
@@ -21,8 +18,5 @@ public class ejer_1 {
         if (!encontrado) {
             System.out.println("el valor no fue encontrado");
         }
-            
-        
-        tc.close();
     }
 }
