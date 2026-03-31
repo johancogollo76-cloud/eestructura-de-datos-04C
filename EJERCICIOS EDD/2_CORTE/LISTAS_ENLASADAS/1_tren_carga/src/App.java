7daf7a6e52d20147420aa6024d1d1a0e2053efa4
johancogollo76-cloud <johancogollo76@gmail.com>
2026-03-31 06:05:51 -0500

    opdate

--- EJERCICIOS EDD/2_CORTE/LISTAS_ENLASADAS/1_tren_carga/src/App.java
+++ EJERCICIOS EDD/2_CORTE/LISTAS_ENLASADAS/1_tren_carga/src/App.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
+// Diseña un sistema para gestionar los vagones de un tren de carga internacional.
 
+// La Clase Vagon (Nodo): Debe contener contenido (String), pesoToneladas (double), origen (String) y destino (String).
+// El Problema: El tren sale de una estación y se le van enganchando vagones al final.
+// Reto: Implementa un método que recorra el tren y calcule el peso total de toda la carga transportada.
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner tc = new Scanner(System.in);
--- EJERCICIOS EDD/2_CORTE/LISTAS_ENLASADAS/2_inventario_de_alimentos/src/App.java
+++ EJERCICIOS EDD/2_CORTE/LISTAS_ENLASADAS/2_inventario_de_alimentos/src/App.java
@@ -1,5 +1,51 @@
+
+// 2. Inventario de Alimentos (Control de Caducidad)
+// Un supermercado necesita gestionar su estante de lácteos. Los productos que vencen más pronto deben colocarse al principio para ser vendidos primero.
+
+// La Clase Producto (Nodo): Debe contener nombre (String), cantidad (int) y diasParaVencer (int).
+// El Problema: Si llega un producto que vence en menos de 3 días, debe insertarse al inicio de la lista (prioridad de venta). Si vence en más tiempo, se pone al final.
+// Reto: Crea un método que imprima solo los productos que tienen menos de 5 días para vencer.
+
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner tc=new Scanner(System.in);
+
+        Stante stan =new Stante();
+
+        int opc;
+        do{
+            System.out.println("MENUU");
+            System.out.println("1.  agregar productos");
+            System.out.println("2.  imprimir");
+            System.out.println();
+            System.out.println("0.  salir");
+            opc=tc.nextInt();
+
+            switch (opc) {
+                case 1:
+                    System.out.println("AGREGAR")   ;
+                    System.out.println("ingrese el nombre del producto: ");
+                    String nombre= tc.nextLine();
+                    System.out.println("ingrese la cantiad: ");
+                    int cantidad=tc.nextInt();
+                    System.out.println("ingrese la can");
+                    int diasParaVencer =tc.nextInt();
+
+                    Producto producto= new Producto(nombre, cantidad, diasParaVencer);
+                    stan.orden_productos(producto);
+                    break;
+                case 2:
+                    System.out.println("IMPRIMIR");
+                    stan.imprimir_productos();
+            
+                default:
+                    break;
+            }
+        }while(opc!=0);
+
+
+        tc.close();
     }
 }
