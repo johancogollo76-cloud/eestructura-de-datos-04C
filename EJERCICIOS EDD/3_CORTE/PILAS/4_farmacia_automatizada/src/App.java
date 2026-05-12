2587d6d0b1871ec553de570ab6d6ab549e2dbede
johancogollo76-cloud <johancogollo76@gmail.com>
2026-05-12 06:49:59 -0500

    update

--- EJERCICIOS EDD/3_CORTE/PILAS/4_farmacia_automatizada/src/App.java
+++ EJERCICIOS EDD/3_CORTE/PILAS/4_farmacia_automatizada/src/App.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        Farmacia farmacia = new Farmacia();
+
+        int opcion;
+        do {
+            System.out.println("\n--- SISTEMA CONTROL DE MEDICAMENTOS    (PILAS) ---");
+            System.out.println("1. Ver los medicamentos");
+            System.out.println("2. Registrar nuevo medicamento (Push)");
+            System.out.println("3. Retirar medicamento del tope (Pop)");
+            System.out.println("4. validar estado de medicamentos");
+            System.out.println("5. Salir");
+            System.out.print("Seleccione una opción: ");
+            opcion = sc.nextInt();
+            sc.nextLine(); // Limpiar buffer
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("\nEstado actual de la pila:");
+                    farmacia.imprimir();
+                    break;
+                case 2:
+                    System.out.print("nombre: "); String nombre = sc.nextLine();
+                    System.out.print("lote: "); String lote = sc.nextLine();
+                    System.out.print("dias para vencer"); int diasParaVencer = sc.nextInt();
+                    sc.nextLine();
+                    
+                    farmacia.push(new Medicamento(nombre, lote, diasParaVencer));
+                    System.out.println("Medicamento apilado correctamente.");
+                    break;
+                case 3:
+                    Medicamento retirado = farmacia.pop();
+                    if (retirado != null) {
+                        System.out.println("Se ha retirado: " + retirado);
+                    } else {
+                        System.out.println("está vacío.");
+                    }
+                    break;
+                case 4:
+                    farmacia.validarDespacho();
+                    break;
+                case 5:
+                    System.out.println("Saliendo del sistema...");
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion != 6);
+        sc.close();
     }
 }
--- EJERCICIOS EDD/3_CORTE/PILAS/4_farmacia_automatizada/src/Farmacia.java
+++ EJERCICIOS EDD/3_CORTE/PILAS/4_farmacia_automatizada/src/Farmacia.java
@@ -1,6 +1,80 @@
 
 public class Farmacia {
     private Medicamento tope;
-    
+    private int tamaño;
+    public Farmacia(){
+        this.tope=null;
+        this.tamaño=0;
+    }
+
+    public boolean esta_vacia(){
+        return tope==null;
+    }
+
+    public int get_tamaño(){
+        return tamaño;
+    }
+
+    public void push(Medicamento nuevo) {
+        nuevo.siguiente = tope;
+        tope = nuevo;
+        tamaño++;
+    }
+
+    public Medicamento pop() {
+        if (esta_vacia()) {
+            return null;
+        }
+        Medicamento aux = tope;
+        tope = tope.siguiente;
+        tamaño--;
+        aux.siguiente = null; 
+        return aux;
+    }
+
+    public Medicamento peek(){
+        return tope;
+    }
+
+    public void imprimir() {
+        if (esta_vacia()) {
+            System.out.println("Pila vacía.");
+            return;
+        }
+        Medicamento actual = tope;
+        System.out.println("=== ESTADO DE MEDICAMENTOS (De Tope a Base) ===");
+        while (actual != null) {
+            if (actual == tope) {
+                System.out.print("  TOPE -> ");
+            } else {
+                System.out.print("          ");
+            }
+            System.out.println(actual.toString());
+            actual = actual.siguiente;
+        }
+        System.out.println("===========================================");
+    }
+
+    public void validarDespacho() {
+
+        while (tope != null && tope.diasParaVencer < 10) {
+
+            Medicamento vencido = pop();
+
+            System.out.println("el medicamento se ha retirado:");
+            System.out.println(vencido);
+        }
+
+        if (tope != null) {
+
+            System.out.println("\nMedicamento seguro :");
+            System.out.println(tope);
+
+        } else {
+
+            System.out.println("\nNo hay medicamentos seguros.");
+        }
+    }
+
 
 }
