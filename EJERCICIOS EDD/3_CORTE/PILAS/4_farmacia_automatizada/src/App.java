c8c3e81961beb7470688ee9d6ca476a23b8052e3
johancogollo76-cloud <johancogollo76@gmail.com>
2026-05-12 07:20:13 -0500

    update

--- EJERCICIOS EDD/3_CORTE/PILAS/4_farmacia_automatizada/src/App.java
+++ EJERCICIOS EDD/3_CORTE/PILAS/4_farmacia_automatizada/src/App.java
@@ -1,3 +1,8 @@
+// En una farmacia de alta tecnología, los medicamentos de alta rotación se almacenan en tubos dispensadores verticales. El personal introduce las cajas por la parte superior y las retira de la misma forma (el último lote en llegar es el primero en ser despachado).
+
+// La Clase Medicamento (Nodo): Debe contener nombre (String), lote (String) y diasParaVencer (int).
+// El Problema: Por norma de seguridad, no se puede despachar un medicamento si le quedan menos de 10 días para vencer.
+// Reto: Implementa un método validarDespacho() que revise el medicamento en el tope. Si está a punto de vencer, debe ser retirado automáticamente y el sistema debe revisar el siguiente. El proceso se repite hasta que el tope sea un medicamento seguro o la pila quede vacía.
 import java.util.Scanner;
 
 public class App {
